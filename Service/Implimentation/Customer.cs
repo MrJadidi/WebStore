10b0ff0d2024025e884b81a070c10acd07e07992
MrJadidi <MrJadidi@gmail.com>
2016-05-02 22:21:19 +1000

    ssd

--- Service/Implimentation/Customer.cs
+++ Service/Implimentation/Customer.cs
@@ -11,7 +11,14 @@ namespace Service.Implimentation
     {
         public void AddCustomer()
         {
-            var cust = new Data.Customers { Name = "Julia", Phone = "0452222366", Address = null, Orders = { OrderID = 1, 2 } };
+            var cust = new Data.Customers { Name = "Julia", Phone = "0452222366", Address = null, Orders = null  };
+            using (var Context = new Data.TestDataEntities())
+            {
+                Context.Customers.Add(cust);
+                Context.SaveChanges(); 
+
+            }
+
         }
 
         public void GetAllCustomer()
